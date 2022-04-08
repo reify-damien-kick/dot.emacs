1e5418ee118274b109da174b25afbb225d9cfd1c
Damien Kick <damien.kick@reifyhealth.com>
2022-04-08 15:11:14 -0500

    Add interactive functions to start test REPLS

--- lib/emacsen/reify.el
+++ lib/emacsen/reify.el
@@ -11,3 +11,15 @@
   (let ((cider-lein-parameters
          "with-profiles +bengal sponsor-repl :headless :host localhost"))
     (cider-jack-in-clj ())))
+
+(defun test-site-cider-jack-in ()
+  (interactive)
+  (let ((cider-lein-parameters
+         "with-profile +test-site repl :headless :host localhost"))
+    (cider-jack-in-clj ())))
+
+(defun test-sponsor-cider-jack-in ()
+  (interactive)
+  (let ((cider-lein-parameters
+         "with-profile +test-sponsor repl :headless :host localhost"))
+    (cider-jack-in-clj ())))
